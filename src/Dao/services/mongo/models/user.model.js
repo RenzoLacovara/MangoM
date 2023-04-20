00bb59af8082273e1ee244352d0f34307a161237
Renzo Lacovara <rnlacovara@gmail.com>
2023-04-20 00:49:00 -0300

    fix

--- src/Dao/services/mongo/models/user.model.js
+++ src/Dao/services/mongo/models/user.model.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 
-// const userCollection = "usuarios";
 const userCollection = "users";
 
 const stringTypeSchemaUniqueRequired = {
@@ -16,7 +15,20 @@ const userSchema = new mongoose.Schema({
   age: Number,
   password: String,
   role: { type: String, default: "user", enum: ["admin", "user"] },
+  cart: {
+    type: [
+      {
+        cartId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "cart",
+        },
+      },
+    ],
+    default: [],
+  },
 });
-
+userSchema.pre("findOne", function () {
+  this.populate("cart.cartId");
+});
 const userModel = mongoose.model(userCollection, userSchema);
 export default userModel;
